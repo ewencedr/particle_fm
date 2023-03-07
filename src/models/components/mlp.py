489235b30cb038ab65ff3862a1240935eadd60aa
Cedric Ewen <cedric.ewen@desy.de>
2023-03-07 19:43:04 +0100

    add first transformer model

--- src/models/components/mlp.py
+++ src/models/components/mlp.py
@@ -56,3 +56,58 @@ class MLP(nn.Module):
         """
 
         return self.net(x)
+
+
+class DiffeqConcat(nn.Module):
+    """
+    Differential equation that concatenates the input and time.
+
+    Args:
+        net (Type[nn.Module]): Neural network that concatenates
+            input `x`, time `t` and `latent` (optional) and outputs
+            the derivative of the same size as `x`.
+    """
+
+    def __init__(self, net):
+        super().__init__()
+        self.net = net
+
+    def forward(self, t, x, latent=None, **kwargs):
+        """
+        Args:
+            t (tensor): Time with shape (..., 1)
+            x (tensor): Input with shape (..., dim)
+            latent (tensor, optional): Latent vector with shape (..., latent_dim).
+                Default: None
+
+        Returns:
+            dx (tensor): Derivative in `x` with shape (..., dim)
+        """
+        t = torch.ones_like(x[..., :1]) * t
+        input = torch.cat([t, x], -1)
+        if latent is not None:
+            input = torch.cat([input, latent], -1)
+        return self.net(input, **kwargs)
+
+
+class DiffeqMLP(DiffeqConcat):
+    """
+    Differential equation defined with MLP.
+
+
+    Args:
+        Same as in `MLP`
+    """
+
+    def __init__(
+        self,
+        in_dim,
+        hidden_dims,
+        out_dim,
+        activation="Tanh",
+        final_activation=None,
+        **kwargs
+    ):
+        super().__init__(
+            MLP(in_dim, hidden_dims, out_dim, activation, final_activation)
+        )
