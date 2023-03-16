fix no normalising in jetnet datamodule

src/data/jetnet_datamodule.py
@@ -152,24 +152,25 @@ class JetNetDataModule(LightningDataModule):
 
             n_samples_val = int(self.hparams.val_fraction * len(x))
             n_samples_test = int(self.hparams.test_fraction * len(x))
-            if self.hparams.normalize:
-                full_mask = np.repeat(mask, repeats=3, axis=-1) == False
-                full_mask = np.ma.make_mask(full_mask, shrink=False)
-                x = np.ma.masked_array(x, full_mask)
-                dataset_train, dataset_val, dataset_test = np.split(
-                    x,
-                    [len(x) - (n_samples_val + n_samples_test), len(x) - n_samples_val],
-                )
+            full_mask = np.repeat(mask, repeats=3, axis=-1) == False
+            full_mask = np.ma.make_mask(full_mask, shrink=False)
+            x_ma = np.ma.masked_array(x, full_mask)
+            dataset_train, dataset_val, dataset_test = np.split(
+                x_ma,
+                [
+                    len(x_ma) - (n_samples_val + n_samples_test),
+                    len(x_ma) - n_samples_val,
+                ],
+            )
 
+            if self.hparams.normalize:
                 means = np.ma.mean(dataset_train, axis=(0, 1))
                 stds = np.ma.std(dataset_train, axis=(0, 1))
 
-                print(f"Means: {means}")
-                print(f"Stds: {stds}")
-                dataset_val2 = np.ma.copy(dataset_val)
+                # print(f"Means: {means}")
+                # print(f"Stds: {stds}")
 
                 normalized_dataset_train = normalize_tensor(dataset_train, means, stds)
-
                 mask_train = np.ma.getmask(normalized_dataset_train) == False
                 mask_train = mask_train.astype(int)
                 mask_train = torch.tensor(np.expand_dims(mask_train[..., 0], axis=-1))
@@ -184,8 +185,8 @@ class JetNetDataModule(LightningDataModule):
 
             # Validation without
 
-            unnormalized_tensor_val = torch.tensor(dataset_val2)
-            unnormalized_mask_val = np.ma.getmask(dataset_val2) == False
+            unnormalized_tensor_val = torch.tensor(dataset_val)
+            unnormalized_mask_val = np.ma.getmask(dataset_val) == False
             unnormalized_mask_val = unnormalized_mask_val.astype(int)
             unnormalized_mask_val = torch.tensor(
                 np.expand_dims(unnormalized_mask_val[..., 0], axis=-1)