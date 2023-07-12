fix calculation of wasserstein metric in eval; remove need for mask in plotting function

src/callbacks/jetnet_eval.py
@@ -174,13 +174,15 @@ class JetNetEvaluationCallback(pl.Callback):
                     : self.num_jet_samples
                 ]
 
+            mask = background_mask
+            cond = background_cond
+
             # maximum number of samples to plot is the number of samples in the dataset
             num_plot_samples = len(background_data)
 
             if self.datasets_multiplier > 1:
-                background_data = np.repeat(background_data, self.datasets_multiplier, axis=0)
-                background_mask = np.repeat(background_mask, self.datasets_multiplier, axis=0)
-                background_cond = np.repeat(background_cond, self.datasets_multiplier, axis=0)
+                mask = np.repeat(mask, self.datasets_multiplier, axis=0)
+                cond = np.repeat(cond, self.datasets_multiplier, axis=0)
 
             # Get EMA weights if available
             if (
@@ -195,10 +197,10 @@ class JetNetEvaluationCallback(pl.Callback):
             # Generate data
             data, generation_time = generate_data(
                 model=pl_module,
-                num_jet_samples=len(background_data),
-                cond=torch.tensor(background_cond),
+                num_jet_samples=len(mask),
+                cond=torch.tensor(cond),
                 variable_set_sizes=trainer.datamodule.hparams.variable_jet_sizes,
-                mask=torch.tensor(background_mask),
+                mask=torch.tensor(mask),
                 normalized_data=trainer.datamodule.hparams.normalize,
                 means=trainer.datamodule.means,
                 stds=trainer.datamodule.stds,
@@ -255,13 +257,12 @@ class JetNetEvaluationCallback(pl.Callback):
                 efps_sim[0],
                 pt_selected_particles_sim[0],
             )
-            sim_data = np.concatenate([background_data, background_mask], axis=-1)
 
             # Plotting
             plot_name = f"{self.model_name}--epoch{trainer.current_epoch}"
             fig = plot_data(
                 particle_data=np.array([data]),
-                sim_data=sim_data,
+                sim_data=background_data,
                 jet_data_sim=jet_data_sim,
                 jet_data=jet_data,
                 efps_sim=efps_sim,

src/callbacks/jetnet_final_eval.py
@@ -176,31 +176,31 @@ class JetNetFinalEvaluationCallback(pl.Callback):
                 : self.num_jet_samples
             ]
         if self.cond_path is not None:
-            background_mask = mask_c[: self.num_jet_samples]
-            background_cond = cond_c[: self.num_jet_samples]
+            mask = mask_c[: self.num_jet_samples]
+            cond = cond_c[: self.num_jet_samples]
+        else:
+            mask = background_mask
+            cond = background_cond
 
-        if len(background_data) != len(background_mask) or len(background_data) != len(
-            background_cond
-        ):
+        if len(background_data) != len(mask) or len(background_data) != len(cond):
             raise ValueError(
-                f"Number of samples in data ({len(background_data)}), mask ({len(background_mask)}) and conditioning ({len(background_cond)}) do not match."
+                f"Number of samples in data ({len(background_data)}), mask ({len(mask)}) and conditioning ({len(cond)}) do not match."
             )
 
         # maximum number of samples to plot is the number of samples in the dataset
         num_plot_samples = len(background_data)
 
         if self.datasets_multiplier > 1:
-            background_data = np.repeat(background_data, self.datasets_multiplier, axis=0)
-            background_mask = np.repeat(background_mask, self.datasets_multiplier, axis=0)
-            background_cond = np.repeat(background_cond, self.datasets_multiplier, axis=0)
+            mask = np.repeat(mask, self.datasets_multiplier, axis=0)
+            cond = np.repeat(cond, self.datasets_multiplier, axis=0)
 
         # Generate data
         data, generation_time = generate_data(
             model=model,
-            num_jet_samples=len(background_data),
-            cond=torch.tensor(background_cond),
+            num_jet_samples=len(mask),
+            cond=torch.tensor(cond),
             variable_set_sizes=trainer.datamodule.hparams.variable_jet_sizes,
-            mask=torch.tensor(background_mask),
+            mask=torch.tensor(mask),
             normalized_data=trainer.datamodule.hparams.normalize,
             means=trainer.datamodule.means,
             stds=trainer.datamodule.stds,
@@ -245,14 +245,12 @@ class JetNetFinalEvaluationCallback(pl.Callback):
             pt_selected_particles_sim[0],
         )
 
-        sim_data = np.concatenate([background_data, background_mask], axis=-1)
-
         # Plotting
         plot_name = f"final_plot{self.suffix}"
         img_path = "/".join(ckpt.split("/")[:-2]) + "/"
         fig = plot_data(
             particle_data=np.array([data]),
-            sim_data=sim_data,
+            sim_data=background_data,
             jet_data_sim=jet_data_sim,
             jet_data=jet_data,
             efps_sim=efps_sim,

src/utils/plotting.py
@@ -462,7 +462,7 @@ def plot_data(
         ax7.set_yscale("log")
 
     ax8 = fig.add_subplot(gs[gs_counter + 1])
-    data1 = np.sum(sim_data[:, :, 3], axis=1)
+    data1 = np.count_nonzero(sim_data[:, :, 2], axis=1)
     if not plot_data_only:
         data = [np.count_nonzero(d[:, :, 2], axis=1) for d in particle_data]
         x_min, x_max = (