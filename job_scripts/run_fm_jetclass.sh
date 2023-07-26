9aae395fc3c17dbd078aa757da4a8d23f40e3675
Joschka Birk <joschka.birk@gmail.com>
2023-07-26 17:01:48 +0200

    Add job script for jetclass

--- job_scripts/run_fm_jetclass.sh
+++ job_scripts/run_fm_jetclass.sh
@@ -12,10 +12,7 @@
 #SBATCH --mail-user joschka.birk@uni-hamburg.de
  
 source ~/.bashrc
- 
-# activate your conda environment the job should use
-# go to your folder with your python scripts
 cd /home/birkjosc/repositories/EPiC-FM/
 
 singularity exec --nv -B /home -B /beegfs /beegfs/desy/user/birkjosc/singularity_images/pytorch-image-v0.0.8.img \
-    bash -c "source /opt/conda/bin/activate && python src/train.py experiment=fm_tops150_cond"
+    bash -c "source /opt/conda/bin/activate && python src/train.py experiment=jetclass"
