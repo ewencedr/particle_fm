fe018fb1c44f710a84ce2c825375aaee8d6a7f8a
Cedric Ewen <cedric.ewen@desy.de>
2023-07-29 01:20:58 +0200

    implement standardisation for lhco data

--- src/data/lhco_datamodule.py
+++ src/data/lhco_datamodule.py
@@ -8,9 +8,18 @@ from torch.utils.data import DataLoader, Dataset, TensorDataset, random_split
 
 from src.utils.pylogger import get_pylogger
 
+from .components import (
+    center_jets,
+    get_base_distribution,
+    mask_data,
+    normalize_tensor,
+    one_hot_encode,
+)
+
 log = get_pylogger("JetNetDataModule")
 
 
+# TODO datadir
 class LHCODataModule(LightningDataModule):
     """LightningDataModule for JetNet dataset. If no conditioning is used, the conditioning tensor
     will be a tensor of zeros.
@@ -58,7 +67,15 @@ class LHCODataModule(LightningDataModule):
         pin_memory: bool = False,
         drop_last: bool = False,
         verbose: bool = True,
+        # data
+        jet_type: str | list[str] = "qcd",
+        num_particles: int = 279,
         variable_jet_sizes: bool = True,
+        conditioning_type: bool = False,
+        # conditioning_pt: bool = True,
+        # conditioning_eta: bool = True,
+        # conditioning_mass: bool = True,
+        # conditioning_num_particles: bool = True,
         # preprocessing
         centering: bool = False,
         normalize: bool = False,
@@ -117,47 +134,174 @@ class LHCODataModule(LightningDataModule):
             mask = mask[:, 0]
             particle_data = particle_data[:, :, [1, 2, 0]]
             particle_data = np.concatenate([particle_data, mask], axis=-1)
-            # data splitting
-            n_samples_val = int(self.hparams.val_fraction * len(particle_data))
-            n_samples_test = int(self.hparams.test_fraction * len(particle_data))
-            dataset_train, dataset_val, dataset_test = np.split(
+
+            # mask and select number of particles, mainly relevant for smaller jet sizes
+            x, mask, masked_particle_data, masked_jet_data = mask_data(
                 particle_data,
+                jet_data,
+                num_particles=self.hparams.num_particles,
+                variable_jet_sizes=self.hparams.variable_jet_sizes,
+            )
+
+            # data splitting
+            n_samples_val = int(self.hparams.val_fraction * len(x))
+            n_samples_test = int(self.hparams.test_fraction * len(x))
+
+            full_mask = np.repeat(mask, repeats=3, axis=-1) == 0
+            full_mask = np.ma.make_mask(full_mask, shrink=False)
+            x_ma = np.ma.masked_array(x, full_mask)
+            dataset_train, dataset_val, dataset_test = np.split(
+                x_ma,
                 [
-                    len(particle_data) - (n_samples_val + n_samples_test),
-                    len(particle_data) - n_samples_test,
+                    len(x_ma) - (n_samples_val + n_samples_test),
+                    len(x_ma) - n_samples_test,
                 ],
             )
+
+            # conditioning
+            # conditioning_data = self._handle_conditioning(jet_data)
+            # if conditioning_data is not None:
+            #    tensor_conditioning = torch.tensor(conditioning_data, dtype=torch.float32)
+            #    conditioning_train, conditioning_val, conditioning_test = np.split(
+            #        conditioning_data,
+            #        [
+            #            len(conditioning_data) - 1 - (n_samples_val + n_samples_test),
+            #            len(conditioning_data) - 1 - n_samples_test,
+            #        ],
+            #    )
+            #    tensor_conditioning_train = torch.tensor(conditioning_train, dtype=torch.float32)
+            #    tensor_conditioning_val = torch.tensor(conditioning_val, dtype=torch.float32)
+            #    tensor_conditioning_test = torch.tensor(conditioning_test, dtype=torch.float32)
+            #
+            #    if len(tensor_conditioning) != len(x_ma):
+            #        raise ValueError("Conditioning tensor and data tensor must have same length.")
+
+            if self.hparams.normalize:
+                means = np.ma.mean(dataset_train, axis=(0, 1))
+                stds = np.ma.std(dataset_train, axis=(0, 1))
+
+                normalized_dataset_train = normalize_tensor(
+                    np.ma.copy(dataset_train), means, stds, sigma=self.hparams.normalize_sigma
+                )
+                mask_train = np.ma.getmask(normalized_dataset_train) == 0
+                mask_train = mask_train.astype(int)
+                mask_train = torch.tensor(np.expand_dims(mask_train[..., 0], axis=-1))
+                tensor_train = torch.tensor(normalized_dataset_train)
+
+                # Validation
+                normalized_dataset_val = normalize_tensor(
+                    np.ma.copy(dataset_val),
+                    means,
+                    stds,
+                    sigma=self.hparams.normalize_sigma,
+                )
+                mask_val = np.ma.getmask(normalized_dataset_val) == 0
+                mask_val = mask_val.astype(int)
+                mask_val = torch.tensor(np.expand_dims(mask_val[..., 0], axis=-1))
+                tensor_val = torch.tensor(normalized_dataset_val)
+
+                # if conditioning_data is not None:
+                #    means_cond = torch.mean(tensor_conditioning_train, axis=0)
+                #    stds_cond = torch.std(tensor_conditioning_train, axis=0)
+            #
+            #    # Train
+            #    tensor_conditioning_train = normalize_tensor(
+            #        tensor_conditioning_train,
+            #        means_cond,
+            #        stds_cond,
+            #        sigma=self.hparams.normalize_sigma,
+            #    )
+            #
+            #    # Validation
+            #    tensor_conditioning_val = normalize_tensor(
+            #        tensor_conditioning_val,
+            #        means_cond,
+            #        stds_cond,
+            #        sigma=self.hparams.normalize_sigma,
+            #    )
+            #
+            #    # Test
+            #    tensor_conditioning_test = normalize_tensor(
+            #        tensor_conditioning_test,
+            #        means_cond,
+            #        stds_cond,
+            #        sigma=self.hparams.normalize_sigma,
+            #    )
+            # else:
+            #    tensor_conditioning_train = torch.zeros(len(dataset_train))
+            #    tensor_conditioning_val = torch.zeros(len(dataset_val))
+            #    tensor_conditioning_test = torch.zeros(len(dataset_test))
+
             tensor_conditioning_train = torch.zeros(len(dataset_train))
             tensor_conditioning_val = torch.zeros(len(dataset_val))
             tensor_conditioning_test = torch.zeros(len(dataset_test))
-            self.data_train = TensorDataset(
-                torch.tensor(dataset_train[:, :, :3], dtype=torch.float32),
-                torch.tensor(np.expand_dims(dataset_train[:, :, 3], axis=-1), dtype=torch.float32),
-                tensor_conditioning_train,
-            )
-            self.data_val = TensorDataset(
-                torch.tensor(dataset_val[:, :, :3], dtype=torch.float32),
-                torch.tensor(np.expand_dims(dataset_val[:, :, 3], axis=-1), dtype=torch.float32),
-                tensor_conditioning_val,
-            )
-            self.data_test = TensorDataset(
-                torch.tensor(dataset_test[:, :, :3], dtype=torch.float32),
-                torch.tensor(np.expand_dims(dataset_test[:, :, 3], axis=-1), dtype=torch.float32),
-                tensor_conditioning_test,
+
+            # Train without normalization
+            unnormalized_tensor_train = torch.tensor(dataset_train)
+            unnormalized_mask_train = np.ma.getmask(dataset_train) == 0
+            unnormalized_mask_train = unnormalized_mask_train.astype(int)
+            unnormalized_mask_train = torch.tensor(
+                np.expand_dims(unnormalized_mask_train[..., 0], axis=-1)
             )
 
-            self.tensor_train = torch.tensor(dataset_train[:, :, :3], dtype=torch.float32)
-            self.mask_train = torch.tensor(
-                np.expand_dims(dataset_train[:, :, 3], axis=-1), dtype=torch.float32
+            # Validation without normalization
+            unnormalized_tensor_val = torch.tensor(dataset_val)
+            unnormalized_mask_val = np.ma.getmask(dataset_val) == 0
+            unnormalized_mask_val = unnormalized_mask_val.astype(int)
+            unnormalized_mask_val = torch.tensor(
+                np.expand_dims(unnormalized_mask_val[..., 0], axis=-1)
             )
-            self.tensor_test = torch.tensor(dataset_test[:, :, :3], dtype=torch.float32)
-            self.mask_test = torch.tensor(
-                np.expand_dims(dataset_test[:, :, 3], axis=-1), dtype=torch.float32
-            )
-            self.tensor_val = torch.tensor(dataset_val[:, :, :3], dtype=torch.float32)
-            self.mask_val = torch.tensor(
-                np.expand_dims(dataset_val[:, :, 3], axis=-1), dtype=torch.float32
-            )
+
+            # Test
+            tensor_test = torch.tensor(dataset_test)
+            mask_test = np.ma.getmask(dataset_test) == 0
+            mask_test = mask_test.astype(int)
+            mask_test = torch.tensor(np.expand_dims(mask_test[..., 0], axis=-1))
+
+            if self.hparams.normalize:
+                self.data_train = TensorDataset(
+                    tensor_train, mask_train, tensor_conditioning_train
+                )
+                self.data_val = TensorDataset(tensor_val, mask_val, tensor_conditioning_val)
+                self.data_test = TensorDataset(tensor_test, mask_test, tensor_conditioning_test)
+
+                self.means = torch.tensor(means)
+                self.stds = torch.tensor(stds)
+
+                # if conditioning_data is not None:
+                #    self.cond_means = means_cond
+                #    self.cond_stds = stds_cond
+            else:
+                self.data_train = TensorDataset(
+                    unnormalized_tensor_train, unnormalized_mask_train, tensor_conditioning_train
+                )
+                self.data_val = TensorDataset(
+                    unnormalized_tensor_val, unnormalized_mask_val, tensor_conditioning_val
+                )
+                self.data_test = TensorDataset(tensor_test, mask_test, tensor_conditioning_test)
+
+                self.means = None
+                self.stds = None
+
+            if self.hparams.verbose:
+                log.info(f"Data of jet types {self.hparams.jet_type} loaded.")
+                # log.info(
+                #    f"Conditioning on {tensor_conditioning_train.shape[-1] if len(tensor_conditioning_train.shape)==2 else 0} variables, consisting of jet_type: {len(self.hparams.jet_type) if self.hparams.conditioning_type else 0}, pt: {1 if self.hparams.conditioning_pt else 0}, eta: {1 if self.hparams.conditioning_eta else 0}, mass: {1 if self.hparams.conditioning_mass else 0}, num_particles: {1 if self.hparams.conditioning_num_particles else 0}"
+                # )
+                if self.hparams.normalize:
+                    log.info(f"{'Training data shape:':<23} {tensor_train.shape}")
+                    log.info(f"{'Validation data shape:':<23} {tensor_val.shape}")
+                    log.info(f"{'Test data shape:':<23} {tensor_test.shape}")
+                    log.info(f"Normalizing data with sigma = {self.hparams.normalize_sigma}")
+                if self.hparams.centering:
+                    log.info("Centering data")
+
+            self.tensor_train = unnormalized_tensor_train
+            self.mask_train = unnormalized_mask_train
+            self.tensor_test = tensor_test
+            self.mask_test = mask_test
+            self.tensor_val = unnormalized_tensor_val
+            self.mask_val = unnormalized_mask_val
             self.tensor_conditioning_train = tensor_conditioning_train
             self.tensor_conditioning_val = tensor_conditioning_val
             self.tensor_conditioning_test = tensor_conditioning_test
