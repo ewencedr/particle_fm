eaab7ff4dc57b7df3f07edc6f4c7baec7a08a1b8
Joschka Birk <joschka.birk@gmail.com>
2023-08-11 18:12:31 +0200

    Adding substructure plots to eval_ckpt.py

--- src/eval_ckpt.py
+++ src/eval_ckpt.py
@@ -1,42 +1,37 @@
 import logging
 import os
 import shutil
-import sys
 from copy import deepcopy
 from pathlib import Path
 
-import cplt
+import h5py
 import hydra
 
 # plots and metrics
-import matplotlib.pyplot as plt
 import numpy as np
-import pytorch_lightning as pl
 import torch
+import yaml
 
 # set env variable DATA_DIR again because of hydra
 from omegaconf import OmegaConf
 
-from src.data.components import (
-    calculate_all_wasserstein_metrics,
-    inverse_normalize_tensor,
-    normalize_tensor,
-)
-from src.data.components.utils import calculate_jet_features
+from src.data.components import calculate_all_wasserstein_metrics
+from src.data.components.metrics import wasserstein_distance_batched
+
+# from src.data.components.utils import calculate_jet_features
 from src.utils.data_generation import generate_data
-from src.utils.plotting import (
+from src.utils.jet_substructure import dump_hlvs
+from src.utils.plotting import (  # create_and_plot_data,; plot_single_jets,
     apply_mpl_styles,
-    create_and_plot_data,
     plot_data,
     plot_full_substructure,
     plot_particle_features,
-    plot_single_jets,
     plot_substructure,
     prepare_data_for_plotting,
 )
 
 # set up logging for jupyter notebook
-pylogger = logging.getLogger()
+pylogger = logging.getLogger("eval_ckpt")
 logging.basicConfig(level=logging.INFO)
 logging.info("test")
 
@@ -45,6 +40,7 @@ apply_mpl_styles()
 # specify here the path to the run directory of the model you want to evaluate
 run_dir = "/beegfs/desy/user/birkjosc/epic-fm/logs/jetclass_cond_jettype/runs/2023-08-10_16-26-03"
 cfg_backup_file = f"{run_dir}/config.yaml"
+evaluate_substructure = True
 
 # load everything from run directory (safer in terms of reproducing results)
 cfg = OmegaConf.load(cfg_backup_file)
@@ -85,16 +81,19 @@ pylogger.info(f"Loaded checkpoint from epoch {ckpt_epoch}")
 
 ckpt_path = Path(ckpt)
 output_dir = (
-    ckpt_path.parent  # this should then be the "evaluated_ckpts" folder
+    ckpt_path.parent
     if f"evaluated_ckpts/epoch_{ckpt_epoch}" in str(ckpt_path)
     else ckpt_path.parent.parent / "evaluated_ckpts" / f"epoch_{ckpt_epoch}"
 )
+pylogger.info(f"Output directory: {output_dir}")
+
 os.makedirs(output_dir, exist_ok=True)
 if not (output_dir / f"epoch_{ckpt_epoch}.ckpt").exists():
     pylogger.info(f"Copy checkpoint file to {output_dir}")
     shutil.copyfile(ckpt, output_dir / f"epoch_{ckpt_epoch}.ckpt")
 
 data_output_path = output_dir / f"generated_data_epoch_{ckpt_epoch}_nsamples_{len(data_sim)}.npz"
+# data_output_path = output_dir / f"generated_data_epoch_{ckpt_epoch}_nsamples_{1000}.npz"
 if data_output_path.exists():
     pylogger.info(
         f"Output file {data_output_path} already exists. "
@@ -177,6 +176,7 @@ pylogger.info("Calculating Wasserstein distances.")
 metrics = calculate_all_wasserstein_metrics(
     data_sim, data_gen
 )  # TODO: should we add a config?, **self.w_dist_config)
+# metrics = {}
 
 # Prepare Data for Plotting
 pylogger.info("Preparing data for plotting.")
@@ -213,7 +213,8 @@ jet_data_sim, efps_sim, pt_selected_particles_sim = (
 # Plotting
 pylogger.info("Plotting distributions.")
 plot_name = f"epoch_{ckpt_epoch}_overview_all_jet_types"
-img_path = output_dir
+img_path = str(output_dir) + "/"
+pylogger.warning(f"Saving plots to {img_path}")
 plot_data(
     particle_data=np.array([data_gen_plotting]),
     sim_data=data_sim,
@@ -240,6 +241,10 @@ if len(jet_types_dict) > 0:
         mask_this_jet_type_sim = cond_sim[:, index] == 1
         # TODO: check if this still works once we use generated conditioning
         mask_this_jet_type_gen = cond_gen[:, index] == 1
+
+        # TODO: the calculation below slows things down a lot
+        # --> can this be done using the calculated values from above and
+        # just apply the mask to the calculated values?
         (
             jet_data_this_jet_type,
             efps_values_this_jet_type,
@@ -287,3 +292,106 @@ if len(jet_types_dict) > 0:
             close_fig=True,
             # **self.plot_config,
         )
+
+if evaluate_substructure:
+    pylogger.info("Calculating substructure.")
+    substructure_path = output_dir
+    substr_filename_gen = "substructure_generated"
+    substructure_full_path = substructure_path / substr_filename_gen
+
+    # calculate substructure for generated data
+    dump_hlvs(data_gen, str(substructure_full_path), plot=False)
+
+    substr_filename_jetclass = "substructure_jetclass"
+    substructure_full_path_jetclass = substructure_path / substr_filename_jetclass
+
+    # calculate substructure for reference data
+    dump_hlvs(data_sim, str(substructure_full_path_jetclass), plot=False)
+
+    # load substructure for model generated data
+    keys = []
+    data_substructure = []
+    with h5py.File(str(substructure_full_path) + ".h5", "r") as f:
+        tau21 = np.array(f["tau21"])
+        tau32 = np.array(f["tau32"])
+        d2 = np.array(f["d2"])
+        for key in f.keys():
+            keys.append(key)
+            data_substructure.append(np.array(f[key]))
+    keys = np.array(keys)
+    data_substructure = np.array(data_substructure)
+
+    # load substructure for JetClass data
+    data_substructure_jetclass = []
+    with h5py.File(str(substructure_full_path_jetclass) + ".h5", "r") as f:
+        tau21_jetclass = np.array(f["tau21"])
+        tau32_jetclass = np.array(f["tau32"])
+        d2_jetclass = np.array(f["d2"])
+        for key in f.keys():
+            data_substructure_jetclass.append(np.array(f[key]))
+    data_substructure_jetclass = np.array(data_substructure_jetclass)
+
+    w_dist_config = {"num_eval_samples": 50_000, "num_batches": 40}
+    # calculate wasserstein distances
+    w_dist_tau21_mean, w_dist_tau21_std = wasserstein_distance_batched(
+        tau21_jetclass, tau21, **w_dist_config
+    )
+    w_dist_tau32_mean, w_dist_tau32_std = wasserstein_distance_batched(
+        tau32_jetclass, tau32, **w_dist_config
+    )
+    w_dist_d2_mean, w_dist_d2_std = wasserstein_distance_batched(d2_jetclass, d2, **w_dist_config)
+
+    # add to metrics
+    metrics["w_dist_tau21_mean"] = w_dist_tau21_mean
+    metrics["w_dist_tau21_std"] = w_dist_tau21_std
+    metrics["w_dist_tau32_mean"] = w_dist_tau32_mean
+    metrics["w_dist_tau32_std"] = w_dist_tau32_std
+    metrics["w_dist_d2_mean"] = w_dist_d2_mean
+    metrics["w_dist_d2_std"] = w_dist_d2_std
+
+    # plot substructure
+    file_name_substructure = "substructure_3plots"
+    file_name_full_substructure = "substructure_full"
+    plot_substructure(
+        tau21=tau21,
+        tau32=tau32,
+        d2=d2,
+        tau21_jetnet=tau21_jetclass,
+        tau32_jetnet=tau32_jetclass,
+        d2_jetnet=d2_jetclass,
+        save_fig=True,
+        save_folder=img_path,
+        save_name=file_name_substructure,
+        close_fig=True,
+        simulation_name="JetClass",
+        model_name="Generated",
+    )
+    plot_full_substructure(
+        data_substructure=data_substructure,
+        data_substructure_jetnet=data_substructure_jetclass,
+        keys=keys,
+        save_fig=True,
+        save_folder=img_path,
+        save_name=file_name_full_substructure,
+        close_fig=True,
+        simulation_name="JetClass",
+        model_name="Generated",
+    )
+
+    # log substructure images
+    img_path_substructure = f"{img_path}{file_name_substructure}.png"
+    img_path_substructure_full = f"{img_path}{file_name_full_substructure}.png"
+
+yaml_path = output_dir / "final_eval_metrics.yml"
+pylogger.info(f"Writing final evaluation metrics to {yaml_path}")
+
+# transform numpy.float64 for better readability in yaml file
+metrics = {k: float(v) for k, v in metrics.items()}
+# write to yaml file
+with open(yaml_path, "w") as outfile:
+    yaml.dump(metrics, outfile, default_flow_style=False)
+
+# rename wasserstein distances for better distinction
+metrics_final = {}
+for key, value in metrics.items():
+    metrics_final[key + "_final"] = value
