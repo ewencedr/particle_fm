93b9aef0d08cb1f5f79c18bbd52d891a825ec8ee
Joschka Birk <joschka.birk@gmail.com>
2023-09-01 18:17:09 +0200

    revert index selection

--- scripts/eval_ckpt.py
+++ scripts/eval_ckpt.py
@@ -412,11 +412,11 @@ def main():
         # calculate substructure for generated data
         if not os.path.isfile(str(substructure_full_path) + ".h5"):
             pylogger.info("Calculating substructure.")
-            dump_hlvs(data_gen[:, :, :3], str(substructure_full_path), plot=False)
+            dump_hlvs(data_gen, str(substructure_full_path), plot=False)
         # calculate substructure for reference data
         if not os.path.isfile(str(substructure_full_path_jetclass) + ".h5"):
             pylogger.info("Calculating substructure.")
-            dump_hlvs(data_sim[:, :, :3], str(substructure_full_path_jetclass), plot=False)
+            dump_hlvs(data_sim, str(substructure_full_path_jetclass), plot=False)
 
         # load substructure for model generated data
         keys = []
