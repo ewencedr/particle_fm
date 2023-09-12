5eb1d459d3786775394e71bd18eafa11284994b3
Cedric Ewen <cedric.ewen@desy.de>
2023-09-12 16:37:52 +0200

    add transformer file back

---
+++ src/models/components/transformer.py
@@ -0,0 +1,190 @@
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+
+
+def mask_(matrices, maskval=0.0, mask_diagonal=True):
+    """Masks out all values in the given batch of matrices where i <= j holds, i < j if
+    mask_diagonal is false In place operation.
+
+    :param tns:
+    :return:
+    """
+
+    b, h, w = matrices.size()
+
+    indices = torch.triu_indices(h, w, offset=0 if mask_diagonal else 1)
+    matrices[:, indices[0], indices[1]] = maskval
+
+
+class SelfAttention(nn.Module):
+    """Canonical implementation of multi-head self attention."""
+
+    def __init__(self, emb, heads=2, mask=False):
+        """
+        :param emb:
+        :param heads:
+        :param mask:
+        """
+
+        super().__init__()
+
+        assert (
+            emb % heads == 0
+        ), f"Embedding dimension ({emb}) should be divisible by nr. of heads ({heads})"
+
+        self.emb = emb
+        self.heads = heads
+        self.mask = mask
+
+        s = emb // heads
+        # - We will break the embedding into `heads` chunks and feed each to a different attention head
+
+        self.tokeys = nn.Linear(emb, emb, bias=False)
+        self.toqueries = nn.Linear(emb, emb, bias=False)
+        self.tovalues = nn.Linear(emb, emb, bias=False)
+
+        self.unifyheads = nn.Linear(emb, emb)
+
+    def forward(self, x):
+        b, t, e = x.size()
+        h = self.heads
+        assert (
+            e == self.emb
+        ), f"Input embedding dim ({e}) should match layer embedding dim ({self.emb})"
+
+        s = e // h
+
+        keys = self.tokeys(x)
+        queries = self.toqueries(x)
+        values = self.tovalues(x)
+
+        keys = keys.view(b, t, h, s)
+        queries = queries.view(b, t, h, s)
+        values = values.view(b, t, h, s)
+
+        # -- We first compute the k/q/v's on the whole embedding vectors, and then split into the different heads.
+        #    See the following video for an explanation: https://youtu.be/KmAISyVvE1Y
+
+        # Compute scaled dot-product self-attention
+
+        # - fold heads into the batch dimension
+        keys = keys.transpose(1, 2).contiguous().view(b * h, t, s)
+        queries = queries.transpose(1, 2).contiguous().view(b * h, t, s)
+        values = values.transpose(1, 2).contiguous().view(b * h, t, s)
+
+        queries = queries / (e ** (1 / 4))
+        keys = keys / (e ** (1 / 4))
+        # - Instead of dividing the dot products by sqrt(e), we scale the keys and values.
+        #   This should be more memory efficient
+
+        # - get dot product of queries and keys, and scale
+        dot = torch.bmm(queries, keys.transpose(1, 2))
+
+        assert dot.size() == (b * h, t, t)
+
+        if self.mask:  # mask out the upper half of the dot matrix, excluding the diagonal
+            mask_(dot, maskval=float("-inf"), mask_diagonal=False)
+
+        dot = F.softmax(dot, dim=2)
+        # - dot now has row-wise self-attention probabilities
+
+        # apply the self attention to the values
+        out = torch.bmm(dot, values).view(b, h, t, s)
+
+        # swap h, t back, unify heads
+        out = out.transpose(1, 2).contiguous().view(b, t, s * h)
+
+        return self.unifyheads(out)
+
+
+class TransformerBlock(nn.Module):
+    def __init__(self, emb, heads, mask, seq_length, ff_hidden_mult=4, dropout=0.0):
+        super().__init__()
+
+        self.attention = SelfAttention(emb, heads=heads, mask=mask)
+
+        self.mask = mask
+
+        self.norm1 = nn.LayerNorm(emb)
+        self.norm2 = nn.LayerNorm(emb)
+
+        self.ff = nn.Sequential(
+            nn.Linear(emb, ff_hidden_mult * emb),
+            # nn.ReLU(),
+            nn.Tanh(),
+            nn.Linear(ff_hidden_mult * emb, emb),
+        )
+
+        self.do = nn.Dropout(dropout)
+
+    def forward(self, x):
+        attended = self.attention(x)
+
+        x = self.norm1(attended + x)
+
+        x = self.do(x)
+
+        fedforward = self.ff(x)
+
+        x = self.norm2(fedforward + x)
+
+        x = self.do(x)
+
+        return x
+
+
+class Transformer(nn.Module):
+    """Transformer for classifying sequences."""
+
+    def __init__(
+        self,
+        emb,
+        heads,
+        depth,
+        seq_length,
+        mask=True,
+        dropout=0.0,
+        input_dim=4,
+        output_dim=3,
+    ):
+        """
+        :param emb: Embedding dimension
+        :param heads: nr. of attention heads
+        :param depth: Number of transformer blocks
+        :param seq_length: Expected maximum sequence length
+        :param num_classes: Number of classes.
+        :param max_pool: If true, use global max pooling in the last layer. If false, use global
+                         average pooling.
+        """
+        super().__init__()
+
+        tblocks = []
+        for _ in range(depth):
+            tblocks.append(
+                TransformerBlock(
+                    emb=emb,
+                    heads=heads,
+                    seq_length=seq_length,
+                    mask=mask,
+                    dropout=dropout,
+                )
+            )
+
+        self.tblocks = nn.Sequential(*tblocks)
+
+        self.do = nn.Dropout(dropout)
+        self.emb = nn.Linear(input_dim, emb)
+        self.demb = nn.Linear(emb, output_dim)
+
+    def forward(self, x):
+        """
+        :param x: A batch by sequence length integer tensor of token indices.
+        :return: predicted log-probability vectors for each token based on the preceding tokens.
+        """
+        x = self.emb(x)
+        x = self.do(x)
+        x = self.tblocks(x)
+        x = self.demb(x)
+
+        return x
