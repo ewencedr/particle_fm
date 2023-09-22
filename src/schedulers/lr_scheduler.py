d7d73cfbef11ab510197a01281a211b168b11b9e
Joschka Birk <joschka.birk@gmail.com>
2023-09-22 16:53:07 +0200

    fix lr scheduler (had negative lr in first epoch)

--- src/schedulers/lr_scheduler.py
+++ src/schedulers/lr_scheduler.py
@@ -57,7 +57,7 @@ class OneCycleCooldown(_LRScheduler):
         super().__init__(optimizer)
 
     def get_lr(self):
-        lr = self.get_lr_factor(epoch=self.last_epoch - 1)
+        lr = self.get_lr_factor(epoch=self.last_epoch)
         return [lr for base_lr in self.base_lrs]
 
     def get_lr_factor(self, epoch):
