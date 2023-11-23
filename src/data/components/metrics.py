875f2ff4d928a6e065e7cde368338a66bc000a5d
Joschka Birk <joschka.birk@gmail.com>
2023-11-23 09:34:44 +0100

    Adding metrics for KLD calculation

--- src/data/components/metrics.py
+++ src/data/components/metrics.py
@@ -157,3 +157,196 @@ def calculate_all_wasserstein_metrics(
         "w1efp_std": np.mean(w1efp_std),
     }
     return w_dists
+
+
+def kl_divergence(p, q, rescale: bool = False, verbose: bool = False):
+    r"""
+    Calculate the Kullback-Leibler divergence between two probability distributions.
+    KLD(P || Q) = \sum_i [ P(i) * log(P(i) / Q(i)) ]
+    (could have also used scipy.stats.entropy, but this implementation is more flexible
+    and allows for (not) rescaling the distributions to sum to 1).
+    The scipy implementation rescales by default.
+
+    Parameters:
+    - p: Target probability distribution P
+    - q: Approximated probability distribution Q
+
+    Returns:
+    - kl_divergence: KL divergence between Q and P
+    """
+    # Ensure that the distributions sum to 1
+    if not np.isclose(np.sum(p), 1):
+        if verbose:
+            print("Warning: Distribution p does not sum to 1")
+            print(f"Sum of p: {np.sum(p)}")
+        if rescale:
+            if verbose:
+                print("Rescaling p to sum to 1")
+            p = p / np.sum(p)
+    if not np.isclose(np.sum(q), 1):
+        if verbose:
+            print("Warning: Distribution q does not sum to 1")
+            print(f"Sum of q: {np.sum(q)}")
+        if rescale:
+            if verbose:
+                print("Rescaling q to sum to 1")
+            q = q / np.sum(q)
+
+    # Avoid division by zero and log(0) by setting the result to 0 when P(i) or Q(i) is 0
+    p_or_q_zero = np.logical_or(p == 0, q == 0)
+    kld = np.sum(np.where(p_or_q_zero, 0, p * np.log(p / q)))
+
+    return kld
+
+
+def histedges_equalN(x, nbin):
+    """Return the edges of nbin equiprobable bins of x.
+
+    Parameters:
+    - x: Array of values to be binned
+    - nbin: Number of bins
+
+    Returns:
+    - edges: Edges of the bins
+    """
+    number_of_points = len(x)
+    # the interp below first fits a curve to the function x(i) which
+    # basically has on the x-axis the indices of the array x and on the
+    # y-axis the values of the (sorted) array x.
+
+    # Then, it interpolates the curve at nbin + 1 points from 0 to number_of_points
+    # which returns the corresponding (linearly interpolated) values of x.
+    return np.interp(
+        x=np.linspace(0, number_of_points, nbin + 1),
+        xp=np.arange(number_of_points),
+        fp=np.sort(x),
+    )
+
+
+def calc_reverse_kld(
+    target,
+    approx,
+    nbins: int = 100,
+    return_pi_qi_bins: bool = False,
+    clip_approx: bool = False,
+    rescale_pq: bool = False,
+    verbose: bool = False,
+):
+    r"""
+    Calculate the reverse KL divergence between two probability distributions.
+    Reverse KL divergence is defined as KL(Q || P) = \sum_i [ Q(i) * log(Q(i) / P(i)) ]
+    (i.e. swap P and Q compared to the normal KL divergence).
+
+    Parameters:
+    - target: Target probability distribution P
+    - approx: Approximated probability distribution Q
+    - nbins: Number of equiprobable bins to use for the KL divergence calculation
+    - return_pi_qi_bins: Return the discrete probability distributions p_i and q_i
+        and the bins used for the calculation
+    - clip_approx: Clip the approximated distribution to the range of the target distribution
+    - rescale_pq: Rescale the distributions to sum to 1
+    - verbose: Print additional information
+
+    Returns:
+    - kld_value: Reverse KL divergence between Q and P
+    - p_i: Discrete probability distribution for P (if return_pi_qi_bins is True)
+    - q_i: Discrete probability distribution for Q (if return_pi_qi_bins is True)
+    - bins: Bins used for the calculation (if return_pi_qi_bins is True)
+    """
+    # first, calculate the bins which have equal number of points
+    # from the target distribution
+    equiprobable_bins = histedges_equalN(target, nbins)
+
+    # calculate discrete probability distributions for those bins
+    p_i = np.histogram(target, bins=equiprobable_bins)[0] / len(target)
+
+    # if specified, clip the approximated distribution to the range of the
+    # target distribution
+    if clip_approx:
+        if verbose:
+            print("Clipping approximated distribution to range of target distribution")
+        approx = np.clip(approx, a_min=equiprobable_bins[0], a_max=equiprobable_bins[-1])
+    q_i = np.histogram(approx, bins=equiprobable_bins)[0] / len(approx)
+
+    # calculate the reverse KL divergence (reverse by swapping p and q
+    # compared to the normal KL divergence)
+    kld_value = kl_divergence(p=q_i, q=p_i, rescale=rescale_pq, verbose=verbose)
+    if return_pi_qi_bins:
+        return kld_value, p_i, q_i, equiprobable_bins
+    return kld_value
+
+
+def reversed_kl_divergence_batched(
+    target: np.array,
+    approx: np.array,
+    num_eval_samples: int,
+    num_batches: int,
+    nbins: int = 100,
+    clip_approx: bool = False,
+    rescale_pq: bool = False,
+    verbose: bool = False,
+):
+    """Calculate the reverse KL divergence between two probability distributions multiple times and
+    return mean and std.
+
+    Parameters:
+    - target: Target probability distribution P
+    - approx: Approximated probability distribution Q
+    - num_eval_samples: Number of samples to use for each KL divergence calculation
+    - num_batches: Number of times to calculate the KL divergence
+    - nbins: Number of equiprobable bins to use for the KL divergence calculation
+    - clip_approx: Clip the approximated distribution to the range of the target distribution
+    - rescale_pq: Rescale the distributions to sum to 1
+    - verbose: Print additional information
+
+    Returns:
+    - reversed_kld_mean: Mean of the reverse KL divergence over all batches
+    - reversed_kld_std: Standard deviation of the reverse KL divergence over all batches
+    """
+
+    seed = 42
+    rng = np.random.default_rng(seed)
+
+    if len(target.shape) > 1:
+        print("Warning: Target distribution has more than one dimension")
+        print(f"target.shape: {target.shape}")
+        print("---> Batches will be selected along first dimension, and then flattened")
+
+    reversed_kld_values = []
+    for _ in range(num_batches):
+        # select random samples from the target and approximated distributions
+        rand1 = rng.choice(len(target), size=num_eval_samples)
+        rand2 = rng.choice(len(approx), size=num_eval_samples)
+        rand_target = target[rand1]
+        rand_approx = approx[rand2]
+        # flatten the arrays if they have more than one dimension
+        # (we want this to happen after the random selection, since we want to select
+        # random samples from the first dimension, and then include all entries
+        # from the other dimensions)
+        # i.e. we want to select random jets, and then include all particles from
+        # those jets
+        if len(rand_target.shape) > 1:
+            rand_target = rand_target.flatten()
+        if len(rand_approx.shape) > 1:
+            rand_approx = rand_approx.flatten()
+        # calculate the reverse KL divergence with equiprobable bins
+        reversed_kld, p_i, q_i, bins = calc_reverse_kld(
+            target=rand_target,
+            approx=rand_approx,
+            nbins=nbins,
+            clip_approx=clip_approx,
+            return_pi_qi_bins=True,
+            rescale_pq=rescale_pq,
+            verbose=verbose,
+        )
+        if np.isnan(reversed_kld):
+            print("Warning: NaN encountered in reversed KL divergence")
+            print(f"p_i: {p_i}")
+            print(f"q_i: {q_i}")
+            print(f"frac: {p_i / q_i}")
+            print(f"logfrac: {np.log(p_i / q_i)}")
+        reversed_kld_values.append(reversed_kld)
+
+    reversed_kld_mean = np.mean(reversed_kld_values)
+    reversed_kld_std = np.std(reversed_kld_values)
+    return reversed_kld_mean, reversed_kld_std
